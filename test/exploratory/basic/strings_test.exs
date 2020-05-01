f6f245110490631b339c429307fde547636c6c73
Oleksii Kshenskyi <30338124+Oleksii-Kshenskyi@users.noreply.github.com>
2020-05-01 07:30:27 -0500

    [0.0.3] issue #3: unit testing setup finished (PR #6)
    [0.0.2 - 0.0.3]
    - Created two dummy unit tests;
    - Created one dummy exploratory test;
    - Tagged unit tests as unit and exploratory as exploratory so Mix could distinguish between them;
    - Changed the Makefile so it could execute all tests, only unit tests or only exploratory tests.

---
+++ test/exploratory/basic/strings_test.exs
@@ -0,0 +1,9 @@
+defmodule DarkHand.Test.Exploratory.Basic.StringsTest do
+  use ExUnit.Case
+
+  @tag :exploratory
+  test "retrieves second character of a string" do
+    assert "string" == "string"
+  end
+
+end
