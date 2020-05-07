7f26fcd7f651f698e7b1f578095aa46e458fe257
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-05-07 23:51:28 +0300

    [0.0.5] Exploratory testing of retrieving the file name from the download URL.

--- test/exploratory/httpoison/requests_test.exs
+++ test/exploratory/httpoison/requests_test.exs
@@ -3,14 +3,31 @@ defmodule DarkHand.Test.Exploratory.HTTPoison.Requests do
 
   @tag :exploratory
   test "downloads a file with an HTTP GET request" do
+    file_name = "image.png"
     %HTTPoison.Response{body: body} = HTTPoison.get!("https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png")
-    File.write!("image.png", body)
-    assert(File.exists?("image.png"))
+    File.write!(file_name, body)
+    assert(File.exists?(file_name))
 
-    %{size: size} = File.stat!("image.png")
+    %{size: size} = File.stat!(file_name)
     assert(size == 3118)
 
-    File.rm!("image.png")
-    assert(!File.exists?("image.png"))
+    File.rm!(file_name)
+    assert(!File.exists?(file_name))
+  end
+
+  @tag :exploratory
+  test "downloads a file and saves it with its original URL name" do
+    the_url = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
+    file_name = Path.basename(the_url)
+
+    %HTTPoison.Response{body: body} = HTTPoison.get!(the_url)
+    File.write!(file_name, body)
+    assert(File.exists?(file_name))
+
+    %{size: size} = File.stat!(file_name)
+    assert(size == 3118)
+
+    File.rm!(file_name)
+    assert(!File.exists?(file_name))
   end
 end
